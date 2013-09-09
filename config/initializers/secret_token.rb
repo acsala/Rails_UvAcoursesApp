50536e34dba55975a4c76b6f9b7c5c153ae68d23
Attila Csala <attilacsala@gmail.com>
2013-09-09 11:50:44 +0200

    rails template added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UvAcoursesApp::Application.config.secret_key_base = 'ddda4884c732c70bc243eeb573828641116798c355d5b0c0c67ff395a18749deba46623ebd2a62a12f403d86d3f5a3029cffca048ee6151b8f76bf018a4c5535'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+UvAcoursesApp::Application.config.session_store :cookie_store, key: '_UvAcoursesApp_session'
