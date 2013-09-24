b83160b7975208f4b60022b6b632d3c0cef5c1cb
Attila Csala <attilacsala@gmail.com>
2013-09-24 19:28:15 +0200

    Search + routes fixed

--- app/controllers/line_courses_controller.rb
+++ app/controllers/line_courses_controller.rb
@@ -40,7 +40,7 @@ class LineCoursesController < ApplicationController
         #:notice => 'Line item was successfully created.') }
         
         # redirect the browser back to the index insted of individual carts
-        format.html { redirect_to(@cart) }
+        format.html { redirect_to(@line_course) }
         
         format.xml  { render :xml => @line_course,
         :status => :created, :location => @line_course }
