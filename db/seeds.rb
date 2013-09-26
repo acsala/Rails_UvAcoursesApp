6b16ca93afc996ad146e41f8c29937414813227a
Attila Csala <attilacsala@gmail.com>
2013-09-26 10:35:19 +0200

    nokogiri added to seeds, not working properly yet

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,26 +8,24 @@
 
 #---
 # encoding: utf-8
+
+require 'rubygems'  
+require 'nokogiri'
+
 Course.delete_all
-Course.create(:Name => 'Chemical Analysis for Forensic Evidence (CAFE)',
-  :Ects => 6,
-  :Institute => 'Masters School of Sciences',
-  :Programme => 'Master\'s in Chemistry, track Analytical Sciences',
-  :Description => 'Analytical chemistry is a pervasive science in the forensic field. Many different types of forensic samples are subjected to chemical    		analysis. The types of samples, sample handling and sample preparation are briefly discussed. Analytical methods are described in two different ways. A technical perspective is offered for those students that are not yet familiar with forensic science. An example-driven forensic perspective is used to illustrate the application of a variety of methods in practice. The possibilities and limitations of analytical methods are discussed. Attention will be paid to special requirements of chemical analysis in a forensic context (quality, traceability, contamination. An important aspect of the course is a meaningful interpretation of the data, based on (Bayesian) statistics. The ultimate objective is to translate analytical data into solid evidence.',
-  )
-# . . .
 
-Course.create(:Name => 'Theorie en praktijk van het vertalen',
-  :Ects => 12,
-  :Institute => 'Taal- en letterkunde',
-  :Programme => 'Keuzevakken en minors Taal- en letterkunde (BA)',
-  :Description => 'De cursus bestaat uit een theoretisch en een praktisch gedeelte. In het theoretische gedeelte wordt een overzicht gegeven van de verschillende soorten vertaling: schriftelijk versus mondeling, interlinguaal versus intralinguaal en intersemiotisch etc. Het historisch overzicht van het denken over vertalen vormt de aanleiding tot een systematisch overzicht van hedendaagse vertaalopvattingen en van terugkerende discussies in de reflectie over het vertalen.',
-  )
-# . . .
+f = File.open("db/examp_course.xml")
+doc = Nokogiri::XML(f)
+f.close
 
-Course.create(:Name => 'Onderwijssociologie',
-  :Ects => 6,
-  :Institute => 'College Pedagogiek, Onderwijskunde en Lerarenopleiding',
-  :Programme => 'Bachelor Onderwijskunde',
-  :Description => 'Algemeen gesproken is de module opgezet als een plaatsing van het onderwijs in wat in de sociologie de dynamiek van de moderniteit wordt genoemd.',
-  )
+doc.xpath("//courses").each do |courses|
+
+		Course.create(:Name => courses.xpath('//course/name').text,
+			:Ects => courses.xpath('//ects').text,
+			:Institute => courses.xpath('//institute').text,
+			:Programme => courses.xpath('//programme/name').text,
+			:Description => courses.xpath('//description').text,
+			)
+			
+end
+
